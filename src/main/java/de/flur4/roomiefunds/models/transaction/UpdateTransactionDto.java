90c69a1c078b181451985aa0deb49839a3e12045
Fatih Çatalkaya <fatih.catalkaya@yahoo.de>
2025-02-23 02:09:40 +0100

    Annotate UpdateTransactionDto with validation annotations

--- src/main/java/de/flur4/roomiefunds/models/transaction/UpdateTransactionDto.java
+++ src/main/java/de/flur4/roomiefunds/models/transaction/UpdateTransactionDto.java
@@ -1,11 +1,14 @@
 package de.flur4.roomiefunds.models.transaction;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
 import java.util.Optional;
 
-public record UpdateTransactionDto(Optional<Long> sourceAccountId,
-                                   Optional<Long> targetAccountId,
-                                   Optional<Integer> amount,
-                                   Optional<LocalDate> valueDate,
-                                   Optional<String> description) {
+public record UpdateTransactionDto(Optional<@NotNull @Positive Long> sourceAccountId,
+                                   Optional<@NotNull @Positive Long> targetAccountId,
+                                   Optional<@NotNull Integer> amount,
+                                   Optional<@NotNull LocalDate> valueDate,
+                                   Optional<@NotNull String> description) {
 }
