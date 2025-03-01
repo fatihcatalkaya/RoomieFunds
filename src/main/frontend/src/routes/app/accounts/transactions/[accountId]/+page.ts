15a9a87a2d13dff97472f00b191f4344f779028a
Christian Schliz <github@foxat.de>
2025-03-01 23:39:24 +0100

    set slut routes as not prerenderable

--- src/main/frontend/src/routes/app/accounts/transactions/[accountId]/+page.ts
+++ src/main/frontend/src/routes/app/accounts/transactions/[accountId]/+page.ts
@@ -1,6 +1,8 @@
 import type { PageLoad } from "./$types";
 import { createFetchQuery } from "./transactionsLoadQuery";
 
+export const prerender = false;
+
 export const load: PageLoad = ({ params }) => {
     return {
         fetchQuery: createFetchQuery(Number(params.accountId))
--- src/main/frontend/src/routes/app/banking/complete/[intSessionId]/+page.ts
+++ src/main/frontend/src/routes/app/banking/complete/[intSessionId]/+page.ts
@@ -1,6 +1,8 @@
 import { getApiAccount, getApiEnablebankingSessionUnfinishedBySessionId, type EnableBankingUnfinishedSession } from "$lib/client";
 import type { PageLoad } from "./$types";
 
+export const prerender = false;
+
 export const load: PageLoad = ({ params }) => {
     const createUnfinishedSessionQuery: () => Promise<EnableBankingUnfinishedSession> = async () => {
         const query = await getApiEnablebankingSessionUnfinishedBySessionId({
