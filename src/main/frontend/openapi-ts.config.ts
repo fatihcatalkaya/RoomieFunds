a26a4b6a6002714434de442c93e2698c73766ecb
Fatih Çatalkaya <fatih.catalkaya@yahoo.de>
2025-07-15 21:39:16 +0200

    Bump npm dependencies

--- src/main/frontend/openapi-ts.config.ts
+++ src/main/frontend/openapi-ts.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-//  input: 'http://100.124.21.155:8080/q/openapi',
-  input: 'http://localhost:8080/q/openapi',
-  output: 'src/lib/client',
-  plugins: ['@hey-api/client-fetch'],
+	input: 'http://localhost:8080/q/openapi',
+	output: 'src/lib/client',
+	plugins: [
+		{
+			name: '@hey-api/client-fetch',
+			baseUrl: ''
+		}
+	]
 });
